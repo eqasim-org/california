63119a694e98228b4e808cac49ce029665c572f8
balacm <milos.balac@ivt.baug.ethz.ch>
2020-08-05 13:08:33 +0200

    add dependency on a branch from eqasim-java

--- matsim/runtime/eqasim.py
+++ matsim/runtime/eqasim.py
@@ -29,7 +29,7 @@ def execute(context):
     # Clone repository and checkout version
     git.run(context, [
         "clone", "https://github.com/eqasim-org/eqasim-java.git",
-        "--branch", "develop",
+        "--branch", "newMNLSF",
         "--single-branch", "eqasim-java",
         "--depth", "1"
     ])
